68d219429592b774d12da6d5bc10d72aabdc38e7
Aashish Tamsya <aashish.tamsya@gmail.com>
2019-06-05 16:22:49 +0530

    Add make it rain business logic

--- make_it_rain/lib/UI/make_it_rain_state.dart
+++ make_it_rain/lib/UI/make_it_rain_state.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:make_it_rain/UI/make_it_rain.dart';
 
 class MakeItRainState extends State<MakeItRain> {
+  int _moneyCounter = 0;
+
+  void _rainMoney() {
+    setState(() {
+      _moneyCounter += 1000;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -11,7 +18,45 @@ class MakeItRainState extends State<MakeItRain> {
         backgroundColor: Colors.green,
       ),
       backgroundColor: Colors.white,
-      body: Center(),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "Get Rich!",
+                    style: TextStyle(color: Colors.grey, fontSize: 34),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "\$ $_moneyCounter",
+                    style: TextStyle(
+                        color: Colors.greenAccent,
+                        fontSize: 34,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: Center(
+                    child: FlatButton(
+                      color: Colors.lightGreen,
+                      textColor: Colors.white,
+                      child: Text(
+                        "Make It Rain!",
+                        style: TextStyle(fontSize: 19),
+                      ),
+                      onPressed: _rainMoney,
+                    ),
+                  ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
