3d0cc6c9b6394c1d70950199a449bedafc322871
Aashish Tamsya <aashish.tamsya@gmail.com>
2019-06-13 16:03:30 +0530

    Rendering post title on listview using builder

--- introparse/lib/main.dart
+++ introparse/lib/main.dart
@@ -7,16 +7,22 @@ import 'package:http/http.dart' as http;
 
 void main() async {
   List<Post> _data = await getJSON();
-  String _body = "";
-  for (int i=0; i< _data.length; i++) {
-    print(_data[i].title);
-    _body += (_data[i].title + "\n");
-  }
 
   runApp(MaterialApp(
     home: Scaffold(
+      appBar: AppBar(title: Text("JSON Parsing - Posts"), backgroundColor: Colors.pinkAccent,),
       body: Center(
-        child: Text(_body),
+        child: ListView.builder(
+            itemCount: _data.length,
+            padding: EdgeInsets.all(16.0),
+            itemBuilder: (BuildContext context, int position) {
+              if (position.isOdd) return Divider();
+              return ListTile(
+                  title: Text(
+                _data[position].title,
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ));
+            }),
       ),
     ),
   ));
@@ -27,7 +33,7 @@ Future<List> getJSON() async {
   http.Response response = await http.get(url);
   final res = json.decode(response.body);
   var list = List<Post>();
-  for(int i=0; i<res.length; i++) {
+  for (int i = 0; i < res.length; i++) {
     list.add(Post.fromJson(res[i]));
   }
   return list;
